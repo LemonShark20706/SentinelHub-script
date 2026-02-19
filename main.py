6752a7e9b3d513371d1c852ec5797a4acf2ead7c
Oláh <adam.olah06@gmail.com>
2026-02-19 10:59:23 +0100

    Add timer and try_tester decorators for performance measurement and error handling

--- main.py
+++ main.py
@@ -10,7 +10,8 @@ from datetime import datetime
 from colorama import Style
 
 import matplotlib.pyplot as plt
-import tifffile as tiff 
+import tifffile as tiff
+import time
 import os
 
 class ConsolColor:
@@ -171,6 +172,34 @@ class date:
 
     def __str__(self) -> str:
         return f"{self._year}-{self._month}-{self._day}"
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start: float = time.time()
+        result = func(*args, **kwargs)
+        end: float = time.time()
+        print(f"Took {end-start:.8f} second\n\n")
+        return result
+    return wrapper
+
+def try_tester(func):
+    def wrapper(*args, **kwargs):
+        print(ConsolColor.PreSetUpColoredTextLine("Operation starting", "s_color"))
+        try:
+            resoult = func(*args, **kwargs)
+
+        except Exception as e:
+            print(ConsolColor.PreSetUpColoredTextLine(f"Invalid operation: {e}", "danger"))
+            return None
+
+        else:
+            print(ConsolColor.PreSetUpColoredTextLine(f"Successful operation is done", "success"))
+
+            return resoult
+
+        finally:
+            print(ConsolColor.PreSetUpColoredTextLine("Operation ended.", "info"))
+    return wrapper
 
 
 class SentinelHubDownloader:
