f4fd77aefd2a0b93ef716dc02738dffab8cff19d
Oláh <adam.olah06@gmail.com>
2026-02-19 10:10:57 +0100

    Implement download method in SentinelHubDownloader class to fetch data using SentinelHubRequest

--- main.py
+++ main.py
@@ -27,3 +27,48 @@ class SentinelHubDownloader:
     
     def getSample(self) -> str:
         return ", ".join([f"sample.{b}" for b in self.getBands()])
+
+    def download(self, polygon_coords: list[tuple[float, float]], resolution: int = 10):
+        polygon = Polygon(polygon_coords)
+        minx, miny, maxx, maxy = polygon.bounds
+        bbox = BBox(bbox=(minx, miny, maxx, maxy), crs=CRS.WGS84)
+
+        width = int((maxx - minx) * 111320 / resolution)
+        height = int((maxy - miny) * 110540 / resolution)
+        size = (width, height)
+
+        bands_js = ", ".join([f'"{b}"' for b in self.getBands()])
+        samples_js = self.getSample()
+        
+        evalscript = f"""
+        //VERSION=3
+        function setup() {{
+            return {{
+                input: [{{
+                    bands: [{bands_js}],
+                    units: "DN"
+                }}],
+                output: {{
+                    id: "default",
+                    bands: {len(self.getBands())},
+                    sampleType: "UINT16"
+                }}
+            }};
+        }}
+
+        function evaluatePixel(sample) {{
+            return [{samples_js}];
+        }}
+        """
+
+        request = SentinelHubRequest(
+            data_folder="output/SentinelHub",
+            evalscript=evalscript,
+            input_data=[SentinelHubRequest.input_data(DataCollection.SENTINEL2_L1C)],
+            responses=[SentinelHubRequest.output_response("default", MimeType.TIFF)],
+            bbox=bbox,
+            size=size,
+            config=self.config
+        )
+
+        response = request.get_data(save_data=True)
