7a459049486a31d29f8fecf18c7a3d3c832fc636
Oláh <adam.olah06@gmail.com>
2026-02-19 10:56:45 +0100

    Add date class for handling date attributes with properties

--- main.py
+++ main.py
@@ -137,6 +137,42 @@ class ConsolColor:
         finalColorEnd = f"{Style.RESET_ALL}"
         return finalColorEnd   
 
+class date:
+    __slots__ = ["_year", "_month", "_day"]
+
+    def __init__(self, year: int, month: int, day: int) -> None:
+        self._year: int = year
+        self._month: int = month
+        self._day: int = day
+
+    @property
+    def year(self):
+        return self._year
+
+    @year.setter
+    def year(self, value: int):
+        self._year = value
+
+    @property
+    def month(self):
+        return self._month
+
+    @month.setter
+    def month(self, value: int):
+        self._month = value
+
+    @property
+    def day(self):
+        return self._day
+
+    @day.setter
+    def day(self, value: int):
+        self._day = value
+
+    def __str__(self) -> str:
+        return f"{self._year}-{self._month}-{self._day}"
+
+
 class SentinelHubDownloader:
     __slots__ = ["config"]
 
@@ -199,7 +235,6 @@ class SentinelHubDownloader:
         )
 
         response = request.get_data(save_data=True)
-
 
 
 if __name__ == "__main__":
