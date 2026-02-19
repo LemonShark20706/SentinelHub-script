d3fe2e26f89cbc468c654f91967ffd9a2ef2e5b4
Oláh <adam.olah06@gmail.com>
2026-02-19 10:08:58 +0100

    Initialize main.py with necessary imports for Sentinel Hub processing

---
+++ main.py
@@ -0,0 +1,13 @@
+from sentinelhub.constants import CRS, MimeType, MosaickingOrder
+from sentinelhub.data_collections import DataCollection
+from sentinelhub.api.process import SentinelHubRequest
+from sentinelhub.config import SHConfig
+from sentinelhub.geometry import BBox
+from shapely.geometry import Polygon
+from typing import Union, Optional
+from dotenv import load_dotenv
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import tifffile as tiff 
+import os
